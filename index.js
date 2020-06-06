04d3333c8d0bb3ebd82b0cd8bf49f7e53ecf950f
purerosefallen <78877@qq.com>
2020-06-06 19:32:23 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const list = require("./list.json")
 ygopro.i18ns["en-us"].invalid_side_rule = "Illegal cards are contained in your side deck.";
 ygopro.i18ns["zh-cn"].invalid_side_rule = "副卡组中包含不允许换入副卡组的卡。";
 
-ygopro.ctos_follow_after("UPDATE_DECK", true, (buffer, info, client, server, datas) => {
+ygopro.ctos_follow_after("UPDATE_DECK", true, async (buffer, info, client, server, datas) => {
 	var room = ROOM_all[client.rid];
 	if (!room || room.duel_stage === ygopro.constants.DUEL_STAGE.BEGIN) { 
 		return false;
